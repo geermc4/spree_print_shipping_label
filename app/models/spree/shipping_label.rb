f82c76803e86a7842035a9862280755ffdf25df1
geermc4 <geermc4@gmail.com>
2013-08-21 09:55:58 -0700

    commited commented croping by accident

--- app/models/spree/shipping_label.rb
+++ app/models/spree/shipping_label.rb
@@ -413,8 +413,8 @@ class Spree::ShippingLabel
   end
 
   def pdf_crop file_name, margins = [0, 0, 0, 0]
-   # tmp_name = "#{(0...10).map{ ('a'..'z').to_a[rand(26)] }.join}.pdf"
-   # `#{Spree::PrintShippingLabel::Config[:pdfcrop]} --margins="#{margins.join(" ")}" #{file_name} #{@path}#{tmp_name} && rm #{file_name} && mv #{@path}#{tmp_name} #{file_name}` unless Spree::PrintShippingLabel::Config[:pdfcrop].blank?
+    tmp_name = "#{(0...10).map{ ('a'..'z').to_a[rand(26)] }.join}.pdf"
+    `#{Spree::PrintShippingLabel::Config[:pdfcrop]} --margins="#{margins.join(" ")}" #{file_name} #{@path}#{tmp_name} && rm #{file_name} && mv #{@path}#{tmp_name} #{file_name}` unless Spree::PrintShippingLabel::Config[:pdfcrop].blank?
   end
 
   def update_tracking_number t_number
