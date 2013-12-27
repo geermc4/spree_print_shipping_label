66b33bd1617b36d7e0fdc8366d4acd56a7fe0552
Ramón Roche <mrpollo@gmail.com>
2013-12-26 16:08:47 -0800

    fix label sent in order controller

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -1,15 +1,15 @@
 Spree::Admin::OrdersController.class_eval do
   def label
+    shipment = Spree::Shipment.find_by_number(params[:shipment_id])
+    label_file = ""
     begin
-      shipment = Spree::Shipment.find_by_number(params[:shipment_id])
       label_file = shipment.selected_shipping_rate.shipping_label # this also updates the tracking by default
     rescue Spree::LabelError => e
       report_errors e
       flash[:error] = e.message
       redirect_to :back
     end
-
-    send_file label_file, :disposition => 'inline', :type => 'application/pdf' unless label.blank?
+    send_file label_file, :disposition => 'inline', :type => 'application/pdf' unless label_file.blank?
   end
 
   private
