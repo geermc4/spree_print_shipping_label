15212d30f7e3ed1439232f02fcd152a9080b79bf
Ramón Roche <mrpollo@gmail.com>
2013-12-27 14:34:25 -0800

    Revert "Exception notification for LabelErrors from API's"
    This reverts commit aef754c9c209e5693b641921610c5d713d0487a1.
    
    Conflicts:
    	app/controllers/spree/admin/orders_controller_decorator.rb

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -5,17 +5,9 @@ Spree::Admin::OrdersController.class_eval do
     begin
       label_file = shipment.selected_shipping_rate.shipping_label # this also updates the tracking by default
     rescue Spree::LabelError => e
-      report_errors e
       flash[:error] = e.message
       redirect_to :back
     end
     send_file label_file, :disposition => 'inline', :type => 'application/pdf' unless label_file.blank?
-  end
-
-  private
-  def report_errors exception
-    @order = Spree::Order.find_by_number!(params[:order_id]) if params[:order_id]
-    exception.set_backtrace("#{@order.number} - #{@order.email}")
-    ExceptionNotifier.notify_exception(exception, :data => request.env["exception_notifier.exception_data"])
   end
 end
