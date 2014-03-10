af4625ebdf4f3f1c1803d768e0e439ba54788091
Ramón Roche <mrpollo@gmail.com>
2014-03-10 15:52:27 -0700

    grab variant weight from inventory unit

--- app/models/spree/shipping_label/endicia.rb
+++ app/models/spree/shipping_label/endicia.rb
@@ -203,8 +203,8 @@ module Spree
     # don't affect the weight calculations
     def shipment_weight
       return self.declared_weight if self.declared_weight
-      self.shipment.inventory_units.map do |variant|
-        weight = variant.try(:weight).to_f
+      self.shipment.inventory_units.map do |unit|
+        weight = unit.variant.try(:weight).to_f
         (weight.zero?) ? 0.01 : weight
       end.compact.sum
     end
